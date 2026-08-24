f33cbf786fb59c61cba65b02c65e8f3e2b6a22b5
Shepherd Johnston <shepherdjohnston@mu-600096.dhcp.missouri.edu>
2026-08-24 12:48:33 -0500

    Finished depth first search.

--- static/scripts/search_algos.js
+++ static/scripts/search_algos.js
@@ -1,4 +1,23 @@
 function depthFirstSearch(x, y) {
-    const stack = [];
-    stack.push(logicGrid[x][y]);
+
+    if (logicGrid[x][y] === logicGrid[goal[0]][goal[1]]) {
+        return true;
+    }
+
+    logicGrid[x][y]["visited"] = true;
+
+    const directions = [[-1, 0], [1, 0], [0, -1], [0, 1]];
+    for (const [dc, dr] of directions) {
+        const nCol = x + dc;
+        const nRow = y + dr;
+
+        if (nCol >= 0 && nCol < col && nRow >= 0 && nRow < row && !logicGrid[nCol][nRow]["visited"]){
+            const found = depthFirstSearch(nCol, nRow);
+
+            if (found) {
+                return true;
+            }
+        }
+    }
+    return false;
 }
